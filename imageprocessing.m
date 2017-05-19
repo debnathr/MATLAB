6dbdde00ea859a707bdd675f7d0eeb947f8d0cb2
Rubi <er.rubidebnath.2014@gmail.com>
2017-05-19 14:39:38 +0530

    "increasing the contrast and saving the image"

--- imageprocessing.m
+++ imageprocessing.m
@@ -1,7 +1,13 @@
 I = imread('download.jpg'); %Read and Display an Image%
 imshow(I)                   %to display the image%
 whos I                      % to Check How the Image Appears in the Workspace%
-I = rgb2gray(I);
+I = rgb2gray(I);            % to convert the RGB image to grayscale%
 imshow(I)
-figure                      % Improve Image Contrast %
+figure                      
 imhist(I)
+I2 = histeq(I);             % Improve Image Contrast%
+figure
+imshow(I2)
+imhist(I2)
+imwrite (I2, 'processed.png'); %Write the Adjusted Image to a Disk File%
+imfinfo('processed.png')
